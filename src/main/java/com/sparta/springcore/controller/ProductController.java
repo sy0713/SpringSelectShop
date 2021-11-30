4b1cc76d8b1b3a3f8da552455ae31e2c66a5db6a
suky0713@kyonngi.ac.kr <suky0713@kyonggi.ac.kr>
2021-11-30 18:35:03 +0900

    springCore

--- src/main/java/com/sparta/springcore/controller/ProductController.java
+++ src/main/java/com/sparta/springcore/controller/ProductController.java
@@ -7,6 +7,7 @@ import com.sparta.springcore.model.UserRoleEnum;
 import com.sparta.springcore.security.UserDetailsImpl;
 import com.sparta.springcore.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -47,17 +48,28 @@ public class ProductController {
 
     // 로그인한 회원이 등록한 관심 상품 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-// 로그인 되어 있는 회원 테이블의 ID
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         Long userId = userDetails.getUser().getId();
-
-        return productService.getProducts(userId);
+        page = page - 1;
+        return productService.getProducts(userId, page, size, sortBy, isAsc);
     }
 
     // (관리자용) 등록된 모든 상품 목록 조회
-    @Secured(value = UserRoleEnum.Authority.ADMIN)
+    @Secured(UserRoleEnum.Authority.ADMIN)
     @GetMapping("/api/admin/products")
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    public Page<Product> getAllProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        page = page -1;
+        return productService.getAllProducts(page, size, sortBy, isAsc);
     }
 }
